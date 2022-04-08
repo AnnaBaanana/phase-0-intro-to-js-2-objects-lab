fa06430c09f07ea6aba74cfe7315ec308742f9fa
Anna Shulyak <ourmac@Ours-iMac.fios-router.home>
2022-04-07 23:30:20 -0400

    commit tree 5af688242279893cfcc71ee01d11fa4395027c2c

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your solution in this file!
+const employee = {
+    name: "Anna",
+    streetAddress: "123 Broadway"
+}
+
+function updateEmployeeWithKeyAndValue(object, key, value) {
+    const newEmp = {...object};
+    newEmp[key] = value;
+    return newEmp
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(object, key, value) {
+    object[key] = value;
+    return object;
+}
+
+function deleteFromEmployeeByKey(object, key) {
+    const newEmpDel = {...object};
+    delete newEmpDel[key];
+    return newEmpDel
+}
+
+function destructivelyDeleteFromEmployeeByKey(object, key) {
+    delete object[key];
+    return object
+}
+
+//const checkEmp = 
+//destructivelyDeleteFromEmployeeByKey(employee, "name");
+//console.log(employee);
